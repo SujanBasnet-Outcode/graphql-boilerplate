d187c1dc92f4573347d09763323d87300ca68da0
SujanBasnet-Outcode <sujan.basnet@outcodesoftware.com>
2023-08-17 17:26:06 +0545

    Feat: dockerized the application

--- src/db/dbConnection.ts
+++ src/db/dbConnection.ts
@@ -2,12 +2,16 @@ import mongoose, { ConnectOptions } from 'mongoose';
 
 export async function connect(): Promise<void> {
 	try {
+		// Add a delay here before connecting (e.g., setTimeout)
+		await new Promise((resolve) => setTimeout(resolve, 3000));
+
 		await mongoose.connect(process.env.MONGO_URI!, {
 			useNewUrlParser: true
 		} as ConnectOptions);
 
 		console.log('Connected to MongoDB');
 	} catch (error) {
+		console.log(error);
 		console.error('Error connecting to MongoDB:', error.message);
 	}
 }
